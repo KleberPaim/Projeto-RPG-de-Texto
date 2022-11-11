b91cb79a14239aa81ddda43a9423ee08fb16acfb
unknown <kleber.p2012@gmail.com>
2022-11-11 00:37:17 -0300

    Atualização JavaScript

--- mecanica.js
+++ mecanica.js
@@ -1,1 +1,19 @@
-let on = document.getElementById("#ClassesOps")
+// Area das variaveis
+function captura(){
+    let raca = document.getElementById('racas');
+    raca = raca.options[raca.selectedIndex].text;
+    console.log(raca);
+    if ( raca == 'Elfo') {
+        let combatente = document.getElementById('Combatentes').setAttribute('disabled','');
+    }
+    if ( raca == 'Anão'){
+        let conjuradores = document.getElementById('Conjuradores').removeAttribute('disabled');
+    
+    } else {
+        let especialsitas = document.getElementById('Especialistas').setAttribute('disabled','');
+    }
+}
+// Codes
+
+
+
