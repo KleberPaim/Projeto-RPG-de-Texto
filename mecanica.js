850627b2ba00df92447f097a9498b8d659e57dac
unknown <kelber.p2012@gmail.com>
2022-11-11 17:49:47 -0300

    Listas no js e Opções de Trilha

--- mecanica.js
+++ mecanica.js
@@ -1,4 +1,10 @@
-// Area das variaveis
+var personagem = [];
+var racas = ['Elfo','Humano','Anão','Tiefling','Draconato'];
+var classe = [classeCombatente,classeEspecialsita,classeConjuradora];
+var classeCombatente = ['Barbaro','Guerreiro','Paladino','Caçador'];
+var classeEspecialsita = ['Bardo','Monge','Ladrão','Artificer'];
+let classeConjuradora = ['Clerigo','Druida','Bruxo','Mago'];
+// Essa função é um evento
 function captura(){
     let raca = document.getElementById('racas');
     raca = raca.options[raca.selectedIndex].text;
@@ -13,7 +19,7 @@ function captura(){
         let especialsitas = document.getElementById('Especialistas').setAttribute('disabled','');
     }
 }
-// Codes
+
 
 
 
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 Organizando ideia e colocando em ação.
 1 - Criar um formlário para que o usuario crie seu personagem. 
+
+Atualização de Projeto 0.4 (11/11/2022)
+- As Trilhas devem ser adicionadas seguindo as regras condicionais. Cada classe terá acesso a 2 trilhas diferentes. 
+Caso o Usuario escolhar Classe x somente as trilhas y e z  apareceram nas opções de tilha. 
+
