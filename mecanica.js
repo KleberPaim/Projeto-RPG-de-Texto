bd60437350f019e6710c7298b905020b98a451ac
unknown <kelber.p2012@gmail.com>
2022-11-12 12:05:07 -0300

    Atualização 0.7 - javascript

--- mecanica.js
+++ mecanica.js
@@ -1,9 +1,7 @@
 var personagem = [];
 var racas = ['Elfo','Humano','Anão','Tiefling','Draconato'];
-var classe = [classeCombatente,classeEspecialsita,classeConjuradora];
-var classeCombatente = ['Barbaro','Guerreiro','Paladino','Caçador'];
-var classeEspecialsita = ['Bardo','Monge','Ladrão','Artificer'];
-let classeConjuradora = ['Clerigo','Druida','Bruxo','Mago'];
+var classe = ['Barbaro','Guerreiro','Paladino','Caçador','Bardo','Monge','Ladrão','Artificer','Clerigo','Druida','Bruxo','Mago'];
+var trilha = ['Caminho da Furia','Caminho do Espirito','Mestre das Armas','Pugilista','Devoto da luz','Devoto das Trevas','Matador de Monstros','Caça conjunta','Escola do conhecimento','Escola do Valor','Punho suave','Elementarista','Batedor','Assassino','Alquimista','Inventor','Juramento da Vida','Juramento da Guerra','Xamã','Guardião da Natureza','Pacto Feerico','Pacto Profano','Mago da Mente','Mago da Evocador']
 // Essa função é um evento
 function captura(){
     let raca = document.getElementById('racas');
@@ -20,6 +18,9 @@ function captura(){
     }
 }
 
-
-
-
+function novaOpicao(){
+    var n = 0
+    var grupo = document.getElementById('sub-cacador');
+    var opicao = document.createElement('option').setAttribute('id',`trilha${trilha[n]}`);
+    document.body.insertBefore(opicao,grupo)
+}
