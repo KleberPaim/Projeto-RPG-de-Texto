9f01ca95a58e7b13aca1aca42610a7895bd65173
unknown <kleber.p2012@gmail.com>
2022-11-21 00:00:33 -0300

    Finalização

--- mecanica.js
+++ mecanica.js
@@ -1,5 +1,6 @@
-var char = [['Elfo','Humano','Anão','Tiefling','Draconato'],['Barbaro','Guerreiro','Paladino','Bardo','Monge','Ladrão','Druida','Bruxo','Mago'],['Caminho da Furia','Caminho do Espirito','Mestre das Armas','Pugilista','Devoto da luz','Devoto das Trevas','Escola do conhecimento','Escola do Valor','Punho suave','Elementarista','Batedor','Assassino','Xamã','Guardião da Natureza','Pacto Feerico','Pacto Profano','Mago da Mente','Mago da Evocador'],['Força','Destreza','Constituição','Inteligencia','Sabedoria','Carisma']];
-var atb = [[],[]]
+var char = [['Elfo','Humano','Anão','Tiefling','Draconato'],['Barbaro','Guerreiro','Paladino','Bardo','Monge','Ladrão','Druida','Bruxo','Mago'],['Caminho da Furia','Caminho do Espirito','Mestre das Armas','Pugilista','Devoto da luz','Devoto das Trevas','Escola do conhecimento','Escola do Valor','Punho suave','Elementarista','Batedor','Assassino','Xamã','Guardião da Natureza','Pacto Feerico','Pacto Profano','Mago da Mente','Mago da Evocador'],['Força','Constituição','Destreza','Inteligencia','Sabedoria','Carisma']];
+var val = [[],[],[]]
+var playerChar = []
 //Cria as Raças pegando as informações da Lista
 for (let r = 0; r < char[0].length; r++) {
     var selecao = document.querySelector('#racas');
@@ -28,7 +29,7 @@ for (let t = 0; t < char[2].length; t++){
 //criar Atributos
 for (let a = 0; a < char[3].length; a++) {
     var selecao = document.querySelector('#atributos');
-    selecao.innerHTML += `<big id='valorbase_${a}'> 00 </big> <small id ='bonus_${a}'> Mod: 0</small> </<label>${char[3][a]}</label><button id="atb_${a}" onclick="bonus(${a},valorBase(diceroll(6,4)))" > 4d6 </button> <br>`;
+    selecao.innerHTML += `<big id='valorbase_${a}'> 00 </big> <small id ='bonus_${a}'> Mod: 0</small> </<label>${char[3][a]}</label><button id="atb_${a}" onclick="vida_Energia(bonus(${a},valorBase(diceroll(3,8))))" > 4d6 </button> <br>`;
     document.body.appendChild(selecao)
 }
 //onchange 
@@ -62,7 +63,6 @@ function bonusDaRaca(){
 function desbloqueiaTrilha(){
     var classe = document.querySelector('#ClasseOps');
     classe = classe.options[classe.selectedIndex].id;
-    console.log(classe);
         switch (classe) {
             case 'c_0':
                 for (let off = 0; off < char[2].length; off++) {
@@ -157,66 +157,96 @@ function valorBase(roll){
     final = roll - Math.min(...rolagem)
     return final
 }
-// onchange pra exibir o atributo
+// valor nos botões e caulcula os bonus
 function bonus(num,final){ 
     const valorBase = document.querySelector(`#valorbase_${num}`);
     const bonus = document.querySelector(`#bonus_${num}`);
     const botao = document.querySelector(`#atb_${num}`);
     valorBase.innerText = `${final}`;
-    atb[0].push(final);
+    val[0].push(final);
     let soma = 0
     if (final == 2 || final == 3){
         bonus.innerText = `Mod |-4|`;
-        atb[1].push(-4);
+        val[1].push(-4);
         botao.setAttribute("disabled", "disabled");
     }if (final == 4 || final == 5){
         bonus.innerText = `Mod |-3|`;
-        atb[1].push(-3);
+        val[1].push(-3);
         botao.setAttribute("disabled", "disabled");
     }if (final == 6 || final == 7){
         bonus.innerText = `Mod |-2|`;
-        atb[1].push(-2);
+        val[1].push(-2);
         botao.setAttribute("disabled", "disabled");
     }if (final == 8 || final == 9){
         bonus.innerText = `Mod |-1|`;
-        atb[1].push(-1);
+        val[1].push(-1);
         botao.setAttribute("disabled", "disabled");
     }if (final == 10 || final == 11){
         bonus.innerText = `Mod |+0|`;
-        atb[1].push(0);
+        val[1].push(0);
         botao.setAttribute("disabled", "disabled");
     }if (final == 12 || final == 13){
         bonus.innerText = `Mod |+1|`;
-        atb[1].push(+1);
+        val[1].push(+1);
         botao.setAttribute("disabled", "disabled");
     }if (final == 14 || final == 15){
         bonus.innerText = `Mod |+2|`;
-        atb[1].push(+2);
+        val[1].push(+2);
         botao.setAttribute("disabled", "disabled");
     }if (final == 16 || final == 17){
         bonus.innerText = `Mod |+3|`;
-        atb[1].push(+3);
+        val[1].push(+3);
         botao.setAttribute("disabled", "disabled");
     }if (final == 18 || final == 19){
-        atb[1].push(+4);
+        val[1].push(+4);
         bonus.innerText = `Mod |+4|`;
         botao.setAttribute("disabled", "disabled");
     }if (final == 20 || final == 21){
-        atb[1].push(+5);
+        val[1].push(+5);
         bonus.innerText = `Mod |+5|`;
         botao.setAttribute("disabled", "disabled");
     }if (final == 22 || final == 23){
-        atb[1].push(+6);
+        val[1].push(+6);
         bonus.innerText = `Mod |+6|`;
         botao.setAttribute("disabled", "disabled");
     } 
-    for(var i = 0; i < atb[1].length; i++) {
-        soma += atb[1][i];
-    }
-    if (soma < 7){
-        menor = Math.min(...atb[1])
-        console.log(menor)
-        console.log(soma)
-    }
-    console.table(atb)
+    // for(var i = 0; i < val[1].length; i++) {
+    //     soma += val[1][i];
+    // }
+    // if (soma < 7){
+    //     menor = Math.min(...val[1])
+    //     console.log(menor)
+    //     console.log(soma)
+    // }
 }   
+// função vai pegar os valores do tipo de classe escolhida e 
+const pv = document.querySelector('#pv');
+    const pe = document.querySelector('#pe');
+    
+function vida_Energia(){
+    const forca = document.querySelector('#valorbase_0').innerText;
+    const constituicao = document.querySelector('#valorbase_1').innerText;
+    var classe = document.querySelector('#ClasseOps');
+    classe = classe.options[classe.selectedIndex].id;
+    soma = Number(forca) + Number(constituicao)
+    if (classe == 'c_0' || classe == 'c_1' || classe == 'c_2'){
+        pv.innerHTML = `${soma+15}`
+        pe.innerHTML = `${5}`
+    }if (classe == 'c_3' || classe == 'c_4' || classe == 'c_5'){
+        pv.innerHTML = `${soma+10}`
+        pe.innerHTML = `${10}`
+    }if (classe == 'c_6' || classe == 'c_7' || classe == 'c_8'){
+        pv.innerHTML = `${soma+5}`
+        pe.innerHTML = `${20}`
+        
+    }
+}
+
+//salva todas as informações.
+function salvar(){
+    var nome = document.querySelector('#name').value;
+    playerChar.splice(0,1,nome);
+    playerChar.splice(1,1,pv.innerText);
+    playerChar.splice(2,1,pe.innerText);
+    console.log(playerChar)
+}
--- readme.md
+++ readme.md
@@ -14,14 +14,14 @@ REGRA DE NEGÓCIO
 Caso o total de seus modificadores não some pelo menos +6, você pode rolar novamente o menor valor obtido.
 
 PV (Pontos de Vida):
-    Valor Base do Atributo Força + Valor Base da constituição + 15 se for combatente
-    Valor Base do Atributo Força + VB da Constituição + 10 se especialista
+    VB de Força + VB de Con + 15 se for combatente
+    VB de Força + VB de Con + 10 se especialista
     VB de Força + VB de Con + 5 se Conjurador
 
 PE (Pontos de Energia) Servem para usar habilidades e magias:
-    Ele pode escolher entre INT , CAR ou SAB como valor base
-    se combatente = VB
-    se Especialista = VB + 5
-    se Conjurador = VB + 15
+São valores fixos
+    se combatente = 5
+    se Especialista = 10
+    se Conjurador = 20
 
 Botão Avançar vai Pegar as informações de Nome, Raça, Classe, Trilha, Nome, ValorBase dos atributos, Modificador dos atributos e Armazenar na matriz Personagens
